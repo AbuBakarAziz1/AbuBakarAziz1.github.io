9e05731f35cd24334374c7496445c0a7ba9dc17c
abubakaraziz1 <abubakaraziz1122@gmail.com>
2024-02-04 15:35:13 +0500

    Minor Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,3 @@
-// const tabs = document.querySelectorAll('.tab');
-// const sections = document.querySelectorAll('section');
-
-// function isElementInViewport(el) {
-//   const rect = el.getBoundingClientRect();
-//   return (rect.top >= 0 && rect.bottom <= window.innerHeight);
-// }
-
-// function updateActiveTab() {
-//   let activeTabFound = false;
-//   sections.forEach((section, index) => {
-//     if (isElementInViewport(section) && !activeTabFound) {
-//       tabs.forEach(tab => tab.classList.remove('text-yellow-400'));
-//       tabs[index].classList.add('text-yellow-400');
-//       activeTabFound = true;
-//     }
-//   });
-// }
-
-// document.addEventListener('scroll', updateActiveTab);
-// window.addEventListener('load', updateActiveTab);
-
-// updateActiveTab();
-
 document.addEventListener("DOMContentLoaded", function () {
   const sections = document.querySelectorAll(".section");
   const navLinks = document.querySelectorAll("#sidebar a");
